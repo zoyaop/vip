2fa102466dba9c30ffebd87d70148c8f3f488556
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-01-16 08:36:00 +0530

    0.3 version

--- VIPMUSIC/utils/inline/start.py
+++ VIPMUSIC/utils/inline/start.py
@@ -1,14 +1,10 @@
-# Copyright (C) 2024 by THE-VIP-BOY-OP@Github, <https://github.com/THE-VIP-BOY-OP>.
-# This file is part of <https://github.com/THE-VIP-BOY-OP/VIP-MUSIC> project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see <https://github.com/THE-VIP-BOY-OP/VIP-MUSIC/blob/master/LICENSE>
-# All rights reserved.
-
 from pyrogram.types import InlineKeyboardButton
+
 import config
+from config import SUPPORT_GROUP
 from VIPMUSIC import app
 
-# 1. Main Start Panel (जब बोट ग्रुप में स्टार्ट हो)
+
 def start_pannel(_):
     buttons = [
         [
@@ -23,12 +19,11 @@ def start_pannel(_):
         ],
         [
             InlineKeyboardButton(text="✡ 𝐆𝚁𝙾𝚄𝙿 ✡", url=config.SUPPORT_GROUP),
-            InlineKeyboardButton(text="🥀 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 🥀", url=config.SUPPORT_CHANNEL),
         ],
     ]
     return buttons
 
-# 2. Private Panel (जब बोट PM में स्टार्ट हो)
+
 def private_panel(_):
     buttons = [
         [
@@ -49,16 +44,12 @@ def private_panel(_):
     ]
     return buttons
 
-# 3. Music Start Panel (यही फंक्शन गायब था - Error Fix)
+
 def music_start_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text="۞ 𝐇𝙴𝙻𝙿 ۞", callback_data="settings_back_helper"
-            ),
-            InlineKeyboardButton(
-                text="☢ 𝐒𝙴𝚃 ☢", callback_data="settings_helper"
-            ),
+            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="☢ 𝐒𝙴𝚃 ☢", callback_data="settings_helper"),
         ],
         [
             InlineKeyboardButton(text="✡ 𝐆𝚁𝙾𝚄𝙿 ✡", url=config.SUPPORT_GROUP),
@@ -66,14 +57,14 @@ def music_start_panel(_):
     ]
     return buttons
 
-# 4. Alive Panel
+
 def alive_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
                 text="✿︎ ᴀᴅᴅ ᴍᴇ ✿︎", url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
         ],
     ]
     return buttons
