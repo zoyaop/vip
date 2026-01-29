a9a4f0db19e5583451a7f25a3e46c2ea67d3c3dd
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <shivampatel425685@gmail.com>
2026-01-29 06:14:14 +0530

    0.3 version

--- config/config.py
+++ config/config.py
@@ -103,7 +103,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com",
+    "https://github.com/KIRU-OP/VIP-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
